e62a0cca2073f09d37fbb1b56407e30caa775dcf
Beatriz Moreira <bbmmegg@gmail.com>
2026-02-11 18:20:55 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,15 +3,23 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
+	
+	private DepartmentService service;
 	
 	private Department entity;
 	
@@ -31,13 +39,35 @@ public class DepartmentFormController implements Initializable {
 	private Button btCancel;
 	
 	@FXML
-	private void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	private void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException ("Entity was null");
+			
+		}
+		if(service == null){
+			throw new IllegalStateException ("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+			
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+
 	@FXML
-	private void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	private void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
@@ -54,6 +84,10 @@ public class DepartmentFormController implements Initializable {
 		this.entity = entity;
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service= service;
+	}
+	
 	public void updateFormData() {
 		if(entity == null) {
 			throw new IllegalStateException("Entity was null");	
@@ -62,4 +96,8 @@ public class DepartmentFormController implements Initializable {
 		txtName.setText(entity.getName());
 		
 	}
+	
+	
+	
+	
 }
